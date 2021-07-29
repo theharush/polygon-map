06bc302f29ecc098cc03d8316fde9685d020fb62
Eyal Harush <theharush@gmail.com>
2021-07-29 14:41:16 +0300

    initial map setup, installed react-bingmaps

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import ReactBingmaps from "react-bingmaps/lib/components/ReactBingmaps";
+import "./map.css";
 
 export const Map = () => {
+    const regularPolygons = [
+        {
+            center: [13.0827, 80.2707],
+            radius: 5,
+            points: 36,
+            option: { fillColor: "rgba(0,0,0,0.5)", strokeThickness: 2 },
+        },
+    ];
+
     return (
-        <div>Hello World</div>
-    )
-}
+        <ReactBingmaps
+            id="one"
+            className="customClass"
+            center={[13.0827, 80.2707]}
+            bingmapKey="AjGOaJOq2-74BkGKZoa2eb6HshtefJ1TrRkLAitQ9aJbb0blKc1BPkQCRwTc4WOD"
+            regularPolygons={regularPolygons}
+        />
+    );
+};
